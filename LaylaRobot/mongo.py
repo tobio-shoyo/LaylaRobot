218038703c3818cd6e20b4f23edef49787d63658
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2021-10-18 22:57:03 +0530

    Update mongo.py

--- LaylaRobot/mongo.py
+++ LaylaRobot/mongo.py
@@ -13,8 +13,6 @@ MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "DaisyX"
 
 
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["Shoyo"]
